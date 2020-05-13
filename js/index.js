81eb4005699f06d19967b235e341fdbda33dbef2
Jeremy Faraone <jeremyfaraone@gmail.com>
2020-05-12 19:21:45 -0700

    eh

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 // Your code goes here
 
-/// mouseover
-
 const fun = document.querySelector('h1')
 fun.addEventListener('mouseover', (e) => {
    fun.style.color = 'orange'
@@ -10,43 +8,41 @@ fun.addEventListener('mouseover', (e) => {
     }, 500);
 });
 
-// mouseenter
-
 const go = document.querySelector('.img-content img')
 go.addEventListener('mouseenter', (e) => {
     go.style.border = '5px solid red'
 });
 
-// mouseleave
-
 go.addEventListener('mouseleave', (e) => {
     go.style.border = 'initial'
 });
 
-// doubleclick
-
 const bus = document.querySelector('.intro img')
 bus.addEventListener('dblclick', () => {
     bus.src = 'img/Brokebus.jpg'
+    bus.style.scale
 });
 
-// click
+const buttons = document.querySelector('.btn')
 
-const buttons = document.querySelector('.destination .btn')
 buttons.addEventListener('click', (e) => {
     buttons.style.color = 'red'
     buttons.style.backgroundColor = 'orange'
 });
 
-// resize
+// const button = document.('.btn')
+
+// buttons.addEventListener('click', (e) => {
+//     buttons.style.color = 'red'
+//     buttons.style.backgroundColor = 'orange'
+// });
 
 const dest = document.querySelector('.content-destination img')
 
 window.addEventListener('resize', (e) => {
     dest.src = 'img/tropical-paradise.jpg'
+    dest.style.border = '5px solid orange'
 });
-
-// scroll
 
 const back = document.querySelector('body')
 
@@ -54,3 +50,23 @@ window.addEventListener('scroll', (e) => {
     back.style.color = 'green'
 });
 
+const image = document.querySelector('.rounded')
+
+image.addEventListener('click', (e) => {
+    image.src = 'img/venice.jpeg'
+});
+
+const image2 = document.querySelector('.rounded')
+
+image2.addEventListener('mouseover', (e) => {
+    image2.style.border = '5px solid blue'
+});
+
+const nav = document.querySelectorAll('a')
+
+nav.forEach(navItem => {
+    navItem.addEventListener('mouseover', event => {
+        event.target.style.color = 'red'
+    });
+});
+event.stopPropagation();
